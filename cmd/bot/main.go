2c524c995f9297d1f47224f352544531ced67917
Ignacio55 <32954193+Ignacio55@users.noreply.github.com>
2023-09-21 20:51:09 +0200

    [update] telegram

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,37 +4,49 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 func main() {
 
 	client := http.Client{}
+	getUpdates := "https://api.telegram.org/bot6194597538:AAGHp55C3b5Alojxh6MYlD6Fix296DWNBQY/getUpdates?offset=%d&timeout=60"
+	urlMessage := "https://api.telegram.org/bot6194597538:AAGHp55C3b5Alojxh6MYlD6Fix296DWNBQY/sendMessage"
 
-	resp, err := client.Get("https://api.telegram.org/bot<TOKEN>/getUpdates")
+	id := 1
+	for {
 
-	if err != nil {
-		fmt.Printf("hello kitty murio :( %s", err)
-		return
+		resp, err := client.Get(fmt.Sprintf(getUpdates, id))
+
+		if err != nil {
+			fmt.Printf("hello kitty murio :( %s", err)
+			return
+		}
+		decoder := json.NewDecoder(resp.Body)
+		response := Response{}
+		err = decoder.Decode(&response)
+		if err != nil {
+			fmt.Printf("hello kitty murio :( %s", err)
+			return
+		}
+		for _, update := range response.Updates {
+			fmt.Printf(" %s", update.Message.Text)
+			http.PostForm(urlMessage, url.Values{
+				"chat_id": []string{fmt.Sprintf("%d", update.Message.Chat.ChatId)},
+				"text":    []string{"fyytytyutytyu"},
+			})
+
+			id = update.UpdateId + 1
+		}
+		time.Sleep(1 * time.Second)
 	}
-
-	//	resp.Body.Read()
-	decoder := json.NewDecoder(resp.Body)
-
-	response := Response{}
-
-	err = decoder.Decode(&response)
-	if err != nil {
-		fmt.Printf("hello kitty murio :( %s", err)
-		return
-	}
-
-	fmt.Printf(" %s", response.Result[0].Message.Text)
 
 }
 
 type Response struct {
-	Ok     bool     `json:"ok,omitempty"`
-	Result []Update `json:"result,omitempty"`
+	Ok      bool     `json:"ok,omitempty"`
+	Updates []Update `json:"result,omitempty"`
 }
 
 type Update struct {
@@ -45,4 +57,8 @@ type Update struct {
 type Message struct {
 	MessageId int    `json:"message_id,omitempty"`
 	Text      string `json:"text,omitempty"`
+	Chat      Chat   `json:"chat,omitempty"`
+}
+type Chat struct {
+	ChatId int `json:"id,omitempty"`
 }
